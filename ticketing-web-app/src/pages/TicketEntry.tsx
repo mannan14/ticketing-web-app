b1e2b282cbaa25159641b41d8362c85b80ccbcac
mannan dassani <mannandassani1@gmail.com>
2024-01-20 13:24:48 +0530

    built

--- ticketing-web-app/src/pages/TicketEntry.tsx
+++ ticketing-web-app/src/pages/TicketEntry.tsx
@@ -1,47 +1,8 @@
-// import { useEffect, useState } from 'react'
-import { useEffect } from 'react'
 import { Navigation, TicketCard } from '../components'
-// import { useSearchParams, useNavigate } from 'react-router-dom';
-// import axios from 'axios';
 import { useTicketContext } from '../components/TicketContext'
 
 const TicketEntry = () => {
   const {tickets, NextPage, PrevPage} = useTicketContext()
-  // const [tickets,setTickets] = useState<{
-  //   topic: string,
-  //   description: string,
-  //   dateCreated: Date,
-  //   severity: string,
-  //   type: string,
-  //   assignedTo: string, // Support Agent ID
-  //   status: "New"|"Assigned"|"Resolved",
-  //   resolvedOn: Date|string
-  // }[]>([])
-  const convertDate = (toDate:Date|string|undefined) => {
-    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    if(toDate){
-        const date = new Date(toDate).getDate();
-        const month = new Date(toDate).getMonth();
-        const year = new Date(toDate).getFullYear();
-        return `${date} ${monthNames[month]} ${year}`
-    }
-    else{
-        return "-"
-    }
-  }
-
-  useEffect(()=>{
-    // Get all the tickets
-    // axios.get(`${import.meta.env.VITE_SERVER_URL}/support-tickets?page=${page}&sort=${sort}`)
-    // .then((res)=>{
-    //     console.log(res)
-    //     // setTickets(res.data)
-    // })
-    // .catch((err)=>{
-    //     console.log(err)
-    // })
-    console.log(tickets)
-  },[])
 
   return (
     <div className='flex flex-col h-screen max-h-screen'>
